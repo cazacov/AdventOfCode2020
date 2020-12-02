3dc4f02e03aab055c435281193767f96e0628e89
Victor Cazacov <cazacov.git@gmail.com>
2020-12-02 21:20:05 +0100

    Day 2, puzzle 1 solved

---
+++ Day_02_1/Password.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day_02_1
+{
+    public class Password {
+        readonly char policyChar;
+        readonly int policyLow;
+        readonly int policyHigh;
+        readonly string password;
+
+        public Password(string rawText)
+        {
+            var r = new Regex(@"(\d*)-(\d*) (\w*): (\w*)");
+            var m = r.Match(rawText);
+            if (!m.Success)
+            {
+                throw new ArgumentOutOfRangeException($"Unsupported input format: {rawText}");
+            }
+            if (m.Groups.Count != 5)
+            {
+                throw new ArgumentOutOfRangeException($"Unsupported input format: {rawText}");
+            }
+            policyLow = int.Parse(m.Groups[1].Value);
+            policyHigh = int.Parse(m.Groups[2].Value);
+            policyChar = m.Groups[3].Value.First();
+            password = m.Groups[4].Value;
+        }
+
+        internal bool IsValid()
+        {
+            int charCount = password.ToCharArray().Where(x => x == policyChar).Count();
+            return charCount >= policyLow && charCount <= policyHigh;
+        }
+    }
+}
---
+++ Day_02_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Day_02_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texts = System.IO.File.ReadAllLines("input.txt");
+            var passwords = texts.ToList().ConvertAll(t => new Password(t));
+            Console.WriteLine(passwords.Count(x => x.IsValid()));
+        }
+    }
+}
