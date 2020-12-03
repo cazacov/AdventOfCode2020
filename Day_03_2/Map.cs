2e4c71ea328a18ff95e903bbfd3015759ccf3a81
Victor Cazacov <cazacov.git@gmail.com>
2020-12-03 06:37:37 +0100

    Day 03 puzzle 2 solved

---
+++ Day_03_2/Map.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+
+namespace Day_03_2
+{
+    public class Map
+    {
+        public int Height { get; private set; }
+        public int Width { get; private set; }
+
+        private bool[,] map;
+
+        public void ReadFromFile(string fileName)
+        {
+            var strings = System.IO.File.ReadAllLines(fileName);
+            this.Height = strings.Length;
+            this.Width = strings.First().Length;
+            this.map = new bool[Height, Width];
+
+            var y = 0;
+            foreach (var str in strings)
+            {
+                var x = 0;
+                foreach (var ch in str.ToCharArray())
+                {
+                    map[y, x++] = (ch == '#');
+                }
+                y++;
+            }
+        }
+
+        public bool HasTree(int x, int y)
+        {
+            x %= this.Width;
+            return y < Height && map[y, x];
+        }
+
+        public long TreeCount(int slope_x, int slope_y)
+        {
+            var counter = 0L;
+            var x = 0;
+            var y = 0;
+            while (y < Height)
+            {
+                if (HasTree(x, y))
+                {
+                    counter++;
+                }
+                x += slope_x;
+                y += slope_y;
+            }
+            return counter;
+        }
+    }
+}
---
+++ Day_03_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day_03_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var map = new Map();
+            map.ReadFromFile("input.txt");
+
+            Console.WriteLine(
+                map.TreeCount(1, 1)
+                * map.TreeCount(3, 1)
+                * map.TreeCount(5, 1)
+                * map.TreeCount(7, 1)
+                * map.TreeCount(1, 2));
+        }
+    }
+}
