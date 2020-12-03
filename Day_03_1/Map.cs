5feea9805a5a69c95ed49f71c36dc154346c23c6
Victor Cazacov <cazacov.git@gmail.com>
2020-12-03 06:33:01 +0100

    Day 03, puzzle 1 solved.

---
+++ Day_03_1/Map.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+
+namespace Day_03_1
+{
+    public class Map
+    {
+        public int Height { get; private set; }
+        public int Width { get; private set; }
+
+        private bool[,] map;
+
+        public void ReadFromFile(string fileName)
+        {
+            var strings = System.IO.File.ReadAllLines(fileName);
+            this.Height = strings.Length;
+            this.Width = strings.First().Length;
+            this.map = new bool[Height, Width];
+
+            var y = 0;
+            foreach (var str in strings)
+            {
+                var x = 0;
+                foreach (var ch in str.ToCharArray())
+                {
+                    map[y,x++] = (ch == '#');
+                }
+                y++;
+            }
+        }
+
+        public bool HasTree(int x, int y)
+        {
+            x %= this.Width;
+            return y < Height && map[y, x];
+        }
+    }
+}
---
+++ Day_03_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Day_03_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var map = new Map();
+            map.ReadFromFile("input.txt");
+
+            var counter = 0;
+            var x = 0;
+            foreach (var y in Enumerable.Range(0,map.Height))
+            {
+                if (map.HasTree(x, y))
+                {
+                    counter++;
+                }
+                x += 3;
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
