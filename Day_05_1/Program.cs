0375ff5ab9cc6e9318f68999422221c3da61e80c
Victor Cazacov <cazacov.git@gmail.com>
2020-12-05 06:19:30 +0100

    Day 05, puzzle 1 solved

---
+++ Day_05_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day_05_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var input = System.IO.File.ReadAllLines("input.txt").ToList();
+            var seats = input.ConvertAll(str => new Seat(str));
+            Console.WriteLine(seats.Max(seat => seat.Id));
+        }
+    }
+}
