37ad75ff31ac900250b7780c73139de1396600ee
Victor Cazacov <cazacov.git@gmail.com>
2020-12-05 06:22:51 +0100

    Day 05, puzzle 2 solved

---
+++ Day_05_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Day_05_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = System.IO.File.ReadAllLines("input.txt").ToList();
+            var seats = input.ConvertAll(str => new Seat(str));
+
+            var min = seats.Min(seat => seat.Id);
+            var max = seats.Max(seat => seat.Id);
+
+            foreach (var id in Enumerable.Range(min, max - min + 1))
+            {
+                if (!seats.Any(seat => seat.Id == id))
+                {
+                    Console.WriteLine(id);
+                }
+            }
+        }
+    }
+}
---
+++ Day_05_2/Seat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Day_05_2
+{
+    internal class Seat
+    {
+        public Seat(string str)
+        {
+            str = str.Replace("F", "0");
+            str = str.Replace("B", "1");
+            str = str.Replace("R", "1");
+            str = str.Replace("L", "0");
+            this.Id = Convert.ToInt32(str, 2);
+        }
+
+        public int Id { get; private set; }
+    }
+}
