83ebb486d7dffe2891502fd12021bf75cc529edb
Victor Cazacov <cazacov.git@gmail.com>
2020-12-06 06:30:51 +0100

    Day 06, puzzle 1 solved

---
+++ Day_06_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day_06_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var groups = new List<string>();
+            var lines = System.IO.File.ReadAllLines("input.txt");
+
+            var str = String.Empty;
+
+            foreach(var s in lines)
+            {
+                if (s == String.Empty)
+                {
+                    var g = str.ToCharArray().Distinct();
+                    groups.Add(new string(g.ToArray()));
+                    str = "";
+                }
+                else
+                {
+                    str += s;
+                }
+            }
+            Console.WriteLine(groups.Sum(x => x.Length));
+        }
+    }
+}
