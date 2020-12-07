577038be43a78f93c5a286a60042851053c2d078
Victor Cazacov <cazacov.git@gmail.com>
2020-12-07 06:50:49 +0100

    Solve day 07, puzzle 2

---
+++ Day_07_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day_07_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rules = ReadRules();
+
+            var outer = new List<string>();
+            var processed = new List<string>();
+            int count = CountInner("shiny gold", outer, rules, processed);
+
+            Console.WriteLine(count);
+        }
+
+        private static int CountInner(string bag, List<string> outer, List<Rule> rules, List<string> processed)
+        {
+            var result = 0;
+            var rule = rules.First(r => r.bag == bag);
+
+            foreach (var nested in rule.content)
+            {
+                result += nested.Value;
+                result += CountInner(nested.Key, outer, rules, processed) * nested.Value;
+            }
+            return result;
+        }
+
+        private static List<Rule> ReadRules()
+        {
+            var result = new List<Rule>();
+            var lines = System.IO.File.ReadAllLines("input.txt");
+
+            foreach (var s in lines)
+            {
+                result.Add(Rule.FromString(s));
+            }
+            return result;
+        }
+    }
+}
---
+++ Day_07_2/Rule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day_07_2
+{
+    public class Rule
+    {
+        public string bag;
+        public Dictionary<string, int> content = new Dictionary<string, int>();
+
+        internal static Rule FromString(string s)
+        {
+            var result = new Rule();
+            var pos = s.IndexOf(" bags contain ");
+            result.bag = s.Substring(0, pos);
+            var tail = s.Substring(pos + 14, s.Length - pos - 14 - 1);
+            if (tail == "no other bags")
+            {
+                result.content.Clear();
+            }
+            else
+            {
+                var parts = tail.Split(",".ToCharArray());
+                foreach (var part in parts)
+                {
+                    var p = part.Trim();
+                    var count = Int32.Parse(p.Substring(0, 1));
+                    var bpos = part.IndexOf("bag");
+                    var key = p.Substring(2, bpos - 3).Trim();
+                    result.content.Add(key, count);
+                }
+            }
+            return result;            
+        }
+    }
+}
