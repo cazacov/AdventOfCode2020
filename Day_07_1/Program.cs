d0bcf43113f4c3c53bcda47fe069e5b18df63f63
Victor Cazacov <cazacov.git@gmail.com>
2020-12-07 06:59:21 +0100

    Refactoring

--- Day_07_1/Program.cs
+++ Day_07_1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Day_07_1
 {
@@ -10,72 +11,30 @@ namespace Day_07_1
             var rules = ReadRules();
 
             var outer = new List<string>();
-            var processed = new List<string>();
-            ListOuter("shiny gold", outer, rules, processed);
+            ListOuter("shiny gold", outer, rules);
             Console.WriteLine(outer.Count);
         }
 
-        private static void ListOuter(string bag, List<string> outer, List<Rule> rules, List<string> processed)
+        private static void ListOuter(string bag, List<string> outer, List<Rule> rules)
         {
             foreach (var rule in rules)
             {
                 if (rule.content.ContainsKey(bag))
                 {
-                    if (!outer.Contains(rule.bug))
+                    if (!outer.Contains(rule.bag))
                     {
-                        outer.Add(rule.bug);
+                        outer.Add(rule.bag);
                     }
-                    if (!processed.Contains(rule.bug))
-                    {
-                        processed.Add(rule.bug);
-                        ListOuter(rule.bug, outer, rules, processed);
-                    }
-
+                    ListOuter(rule.bag, outer, rules);
                 }
             }
         }
 
         private static List<Rule> ReadRules()
         {
-            var result = new List<Rule>();
-            var lines = System.IO.File.ReadAllLines("input.txt");
-
-            foreach (var s in lines)
-            {
-                result.Add(Rule.FromString(s));
-            }
-            return result;
-        }
-    }
-
-    public class Rule
-    {
-        public string bug;
-        public Dictionary<string, int> content = new Dictionary<string, int>();
-
-        internal static Rule FromString(string s)
-        {
-            var result = new Rule();
-            var pos = s.IndexOf(" bags contain ");
-            result.bug = s.Substring(0, pos);
-            var tail = s.Substring(pos + 14, s.Length - pos - 14 - 1);
-            if (tail == "no other bags")
-            {
-                result.content.Clear();
-            }
-            else
-            {
-                var parts = tail.Split(",".ToCharArray());
-                foreach (var part in parts)
-                {
-                    var p = part.Trim();
-                    var count = Int32.Parse(p.Substring(0, 1));
-                    var bpos = part.IndexOf("bag");
-                    var key = p.Substring(2, bpos - 3).Trim();
-                    result.content.Add(key, count);
-                }
-            }
-            return result;            
+            var lines = System.IO.File.ReadAllLines("input.txt").ToList();
+            return lines.ConvertAll(line => Rule.FromString(line))
+                .ToList();
         }
     }
 }
