a6e3bf082627a6e27088754b052f4b46da3dcc5b
Victor Cazacov <cazacov.git@gmail.com>
2020-12-17 06:45:44 +0100

    Day 17, puzzle 2

---
+++ Day_17_2/Cube.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Day_17_2
+{
+    public class Cube
+    {
+        public int X;
+        public int Y;
+        public int Z;
+        public int W;
+
+        public Cube(int x, int y, int z, int w)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            W = w;
+        }
+
+        protected bool Equals(Cube other)
+        {
+            return X == other.X && Y == other.Y && Z == other.Z && W == other.W;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Cube)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y, Z, W);
+        }
+
+        public override string ToString()
+        {
+            return $"{X},{Y},{Z},{W}";
+        }
+    }
+}
---
+++ Day_17_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day_17_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new Conway4D();
+            game.Init("input.txt");
+
+            for (var i = 0; i < 6; i++)
+            {
+                game.Next();
+                Console.WriteLine("New generation");
+            }
+            Console.WriteLine(game.ActiveCells());
+        }
+    }
+}
