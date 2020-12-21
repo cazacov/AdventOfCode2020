f95eb4a44162b705322b5c2cb451b3e34b0dbc42
Victor Cazacov <cazacov.git@gmail.com>
2020-12-21 10:35:20 +0100

    Day 21, puzzle 2 solved

---
+++ Day_21_2/Food.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day_21_2
+{
+    class Food
+    {
+        public List<string> Ingredients;
+        public List<string> Allergens = new List<string>();
+
+        public Food(string str)
+        {
+            var head = str;
+            var pos = str.IndexOf("(contains");
+            if (pos > 0)
+            {
+                head = str.Substring(0, pos).Trim();
+            }
+            this.Ingredients = head.Split(' ').ToList();
+            if (pos > 0)
+            {
+                var tail = str.Substring(pos + 9);
+                tail = tail.Substring(0, tail.Length - 1).Trim();
+                this.Allergens = tail.Split(", ", StringSplitOptions.RemoveEmptyEntries).ToList();
+            }
+        }
+    }
+}
---
+++ Day_21_2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day_21_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foods = new List<Food>();
+            var lines = System.IO.File.ReadAllLines("input.txt");
+
+            foods = lines.ToList().ConvertAll(l => new Food(l)).ToList();
+
+            var allAllergens = foods.SelectMany(f => f.Allergens).Distinct().ToList();
+            var allIngredients = foods.SelectMany(f => f.Ingredients).Distinct().ToList();
+
+            var alergToIng = new Dictionary<string, HashSet<string>>();
+            foreach (var allergen in allAllergens)
+            {
+                alergToIng[allergen] = new HashSet<string>(allIngredients);
+            }
+
+            foreach (var food in foods)
+            {
+                foreach (var allergen in food.Allergens)
+                {
+                    alergToIng[allergen].RemoveWhere(i => !food.Ingredients.Contains(i));
+                }
+            }
+
+            var canContain = new HashSet<string>();
+            foreach (var pair in alergToIng)
+            {
+                canContain.UnionWith(pair.Value);
+                Console.WriteLine($"{pair.Key} can be contained in {String.Join(",", pair.Value)}");
+            }
+
+            var cannotContain = new HashSet<string>(allIngredients);
+            cannotContain.RemoveWhere(ing => canContain.Contains(ing));
+
+            var count = foods
+                .Sum(food => 
+                    food.Ingredients.Count(i => cannotContain.Contains(i)));
+            Console.WriteLine($"Ingredients cannot possibly contain any of the allergens appear {count} times");
+
+            bool improved;
+            do
+            {
+                improved = false;
+                foreach (var pair in alergToIng.Where(pp => pp.Value.Count == 1))
+                {
+                    var toBeRemoved = pair.Value.First();
+                    foreach (var pair2 in alergToIng)
+                    {
+                        if (pair2.Key != pair.Key)
+                        {
+                            improved |= pair2.Value.RemoveWhere(s => s == toBeRemoved) > 0;
+                        }
+                    }
+                    if (improved)
+                    {
+                        break;
+                    }
+                }
+            } while (improved);
+
+            var result = String.Join(",",
+                alergToIng
+                    .OrderBy(p => p.Key)
+                    .Select(pp => pp.Value.First()));
+            Console.WriteLine($"Canonical dangerous ingredient list: {result}");
+         }
+    }
+}
