a4b4c840617bcfd2e156cbce63db0b78a9bd350f
Victor Cazacov <cazacov.git@gmail.com>
2020-12-21 07:18:31 +0100

    Day 21, puzzle 1

---
+++ Day_21_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day_21_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foods = new List<Food>();
+            var lines = System.IO.File.ReadAllLines("input.txt");
+
+            foods = lines.ToList().ConvertAll(l => new Food(l)).ToList();
+
+        }
+    }
+}
